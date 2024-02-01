a9b65f4f6ef1aba68be831d5e4541d2fcb28f8de
Eric <f102041332@gmail.com>
2024-02-01 17:07:13 +0800

    [fixed] reptile price

--- rule.js
+++ rule.js
@@ -1,12 +1,13 @@
-import { hasJPUpdate } from './api/reptileRules.js';
-import schedule from 'node-schedule';
+import { reptilePrice } from "./api/reptilePrice.js";
+import { hasJPUpdate } from "./api/reptileRules.js";
+import schedule from "node-schedule";
 
 const main = async () => {
-  schedule.scheduleJob('scheduleReptileRules', '10 17 22 * * 3', async () => {
-    console.log('Start');
-    await hasJPUpdate();
-    console.log('OK!');
-  });
+	schedule.scheduleJob("scheduleReptileRules", "10 02 10 * * 4", async () => {
+		console.log("Start");
+		await hasJPUpdate();
+		console.log("OK!");
+	});
 };
 
 main();
--- test.js
+++ test.js
@@ -1,13 +1,20 @@
-import MongooseCRUD from './api/MongoDb/Api.js';
-import fs from 'fs';
-import { noJp } from './api/reptileRules.js';
-import { reptilePrice } from './api/reptilePrice.js';
+import MongooseCRUD from "./api/MongoDb/Api.js";
+import fs from "fs";
+import { hasJPUpdate, noJp } from "./api/reptileRules.js";
+import { reptilePrice } from "./api/reptilePrice.js";
 // const main = async () => {
-//   reptilePrice();
+// 	const cards = await MongooseCRUD("R", "cards", { star: "星數" });
+
+// 	for (let i = 0; i < cards.length; i++) {
+// 		const card = cards[i];
+// 		await MongooseCRUD("Uo", "cards", { _id: card._id }, { star: "" });
+// 	}
 // };
 
 const main = async () => {
-  noJp();
+	console.log("Start");
+	await hasJPUpdate();
+	console.log("End");
 };
 
 main();
